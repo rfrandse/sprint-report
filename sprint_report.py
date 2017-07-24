685aad8e26a3dfb2e43d6bfc5b5d22027a52d3dd
Reed Frandsen <rfrandse@us.ibm.com>
2017-07-24 13:47:42 -0500

    Add team reporting Signed-off-by: Reed Frandsen <rfrandse@us.ibm.com>

--- sprint_report.py
+++ sprint_report.py
@@ -8,6 +8,14 @@ import requests
 import datetime
 import csv
 import sys
+import re
+import time
+from datetime import timedelta
+import dateutil.parser
+import pytz
+
+from slacker import Slacker
+slack = Slacker(config.token)
 
 
 OPENBMC_REPO_ID = '42542702'
@@ -20,9 +28,36 @@ GITHUB_API_ISSUES = 'https://api.github.com/repos/openbmc/openbmc/issues/'
 
 option_csv = None
 option_pipe = None
+#current_date = datetime.datetime.now()
+current_date = (datetime.datetime.now()).date()
+
+day_late =  (dateutil.parser.parse(str(current_date + timedelta(days=-1)))).replace(tzinfo=pytz.UTC)
+two_day_late = (dateutil.parser.parse(str(current_date + timedelta(days=-2)))).replace(tzinfo=pytz.UTC)
+really_late = (dateutil.parser.parse(str(current_date + timedelta(days=-3)))).replace(tzinfo=pytz.UTC)
+
+if current_date.weekday() == 0:
+    day_late =  (dateutil.parser.parse(str(current_date + timedelta(days=-3)))).replace(tzinfo=pytz.UTC)
+    two_day_late = (dateutil.parser.parse(str(current_date + timedelta(days=-4)))).replace(tzinfo=pytz.UTC)
+    really_late = (dateutil.parser.parse(str(current_date + timedelta(days=-5)))).replace(tzinfo=pytz.UTC)
+if current_date.weekday() == 1:
+    two_day_late = (dateutil.parser.parse(str(current_date + timedelta(days=-4)))).replace(tzinfo=pytz.UTC)
+    really_late = (dateutil.parser.parse(str(current_date + timedelta(days=-5)))).replace(tzinfo=pytz.UTC)
+if current_date.weekday() == 2:
+    really_late = (dateutil.parser.parse(str(current_date + timedelta(days=-5)))).replace(tzinfo=pytz.UTC)
 
 
-def do_report(args):
+phase_list = ['Phase 3',
+            'Phase 4',
+            'Phase 5',
+            'Phase 5.1',
+            'Phase 6',
+            'Phase 7']
+
+
+def do_team_report(args):
+    owner_list = {}
+    stat_list = {}
+
     issueNumber = str(args.e)
     issue_api_url = GITHUB_API_ISSUES + issueNumber
 
@@ -31,7 +66,139 @@ def do_report(args):
     issue_zen_api_url = ZENHUB_API_ISSUES + issueNumber
     zenhub_data = requests.get(issue_zen_api_url, headers=config.zen_auth, verify=False).json()
 
-    print("report:%s") % CTM
+#    return
+#    print("team report:%s") % CTM
+#    print("----")
+#    print github_data['title']
+    slack_message = ("team report:%s") % CTM
+    slack_message += "\n----\n"
+    slack_message += "_<https://github.com/openbmc/openbmc/issues/%s|#%s> %s_\n" % (issueNumber, issueNumber, github_data['title'])
+#    slack_message += github_data['title']
+    slack_message += "\n"
+
+
+    if zenhub_data['is_epic']:
+        issue_zen_api_epic_url = ZENHUB_API_EPICS + issueNumber
+        zen_epic =  requests.get(issue_zen_api_epic_url, headers=config.zen_auth, verify=False).json()
+
+        pt_total = 0
+        for story in zen_epic['issues']:
+            print ("processing %s") % story['issue_number']
+            issue_api_url = GITHUB_API_ISSUES + str(story['issue_number'])
+            github_story_data =  requests.get(issue_api_url, auth=GITHUB_AUTH).json()
+#            print json.dumps(github_story_data, indent=4)
+            issue_owner = 'unknown'
+            if github_story_data['assignee'] is not None:
+                issue_owner = (github_story_data['assignee']['login'])
+
+
+            estimate = story.get('estimate')
+            estimate_value = ''
+            if estimate is not None:
+                estimate_value = estimate.get('value')
+
+            pipeline_name = ''
+            pipeline_state = story.get('pipeline')
+            if pipeline_state is not None:
+                pipeline_name = pipeline_state['name']
+            if github_story_data['state'] == 'closed':
+                pipeline_name = 'Closed'
+            bug_icon = ""
+            phase_label = ""
+            for label in github_story_data['labels']:
+                if label['name'] == "bug":
+                    bug_icon = ":bug:"
+                if label['name'] in phase_list:
+                    phase_label += label['name']
+
+            comment_message = ''
+            stat_message = "*<https://github.com/openbmc/openbmc/issues/%s|#%s> estimate:%s owner:%s state:%s* " % (
+                    story['issue_number'], story['issue_number'], estimate_value,issue_owner, pipeline_name)
+
+            no_status_message = "No STATUS :interrobang:"
+
+            if github_story_data['state'] == 'closed':
+                comment_message = "> :fireworks: -- Closed--\n"
+                no_status_message = ""
+                stat_message += ":fireworks:"
+            elif github_story_data['comments'] > 0:
+                story_comments = requests.get(github_story_data['comments_url'], auth=GITHUB_AUTH).json()
+
+                for comment in story_comments:                    
+                    if "**STATUS**" in comment['body']:
+                        no_status_message = ""
+                        comment_date = dateutil.parser.parse(comment['updated_at'])
+
+                        icon = ":fire::fire::fire:"
+                        if comment_date > day_late:
+                            icon =":white_check_mark:"
+                        elif comment_date > two_day_late:
+                            icon = ":warning:"
+                        elif comment_date > really_late:
+                            icon = ":fire::fire:"
+
+
+                        stat_message += icon
+
+                        comment_message += "Last updated: %s %s\n"  % (comment['updated_at'], icon)
+                        comment_message += comment['body']
+                        comment_message += "\n\n\n"
+
+            comment_message += no_status_message
+            comment_message += "\n"
+            stat_message += no_status_message
+            stat_message += phase_label
+            message =  "*<https://github.com/openbmc/openbmc/issues/%s|#%s> %s* %s\n" % (
+                    story['issue_number'], story['issue_number'], github_story_data['title'].encode('utf-8'),bug_icon)
+            message += "owner:%s estimate:%s %s state:%s\n" % (issue_owner, estimate_value, phase_label, pipeline_name)
+            message += comment_messagell
+
+            owner_list.setdefault(issue_owner, []).append(message)            
+            stat_list.setdefault(issue_owner, []).append(stat_message)
+
+        slack_channel = '#openbmc_sprint_report'
+        slack_channel = '@rfrandse'
+        slack.chat.post_message(slack_channel, slack_message)
+
+
+        sorted_owner_list =  sorted(owner_list.items(), key=lambda x: (x[0],x[1]))
+
+        for owner_name, payload_message in sorted_owner_list:
+            slack_message = "\n********\n%s\n~~~~\n>>>" % owner_name
+            for data in payload_message:
+                slack_message += data
+                slack_message + "\n"
+            slack_message += "\n"
+            slack.chat.post_message(slack_channel, slack_message)
+
+        slack_message = "Status Key\n"
+        slack_message +=":white_check_mark: = Update Status Current\n"
+        slack_message += ":warning:= last updated 1 day\n"
+        slack_message += ":fire::fire:= last updated 2 days\n"
+        slack_message += ":fire::fire::fire: = updated 3+ days ago\n\n"
+            
+        sorted_stat_list =  sorted(stat_list.items(), key=lambda x: (x[0],x[1]))
+        for owner_name, payload_stat_message in sorted_stat_list:
+            for data in payload_stat_message:
+                slack_message += data
+                slack_message += "\n"
+        slack.chat.post_message(slack_channel, slack_message)
+
+    else:
+        print "ERROR: <%s> %s is NOT an epic" % ( issueNumber, github_data['title'])
+
+
+
+def do_epic_report(args):
+    issueNumber = str(args.e)
+    issue_api_url = GITHUB_API_ISSUES + issueNumber
+
+    github_data =  requests.get(issue_api_url, auth=GITHUB_AUTH).json()
+
+    issue_zen_api_url = ZENHUB_API_ISSUES + issueNumber
+    zenhub_data = requests.get(issue_zen_api_url, headers=config.zen_auth, verify=False).json()
+
+    print("epic report:%s") % CTM
     print("----")
     print github_data['title']
     if zenhub_data['is_epic']:
@@ -42,7 +209,7 @@ def do_report(args):
         for story in zen_epic['issues']:
             issue_api_url = GITHUB_API_ISSUES + str(story['issue_number'])
             github_story_data =  requests.get(issue_api_url, auth=GITHUB_AUTH).json()
-            
+
             estimate = story.get('estimate')
             estimate_value = ''
             if estimate is not None:
@@ -51,6 +218,7 @@ def do_report(args):
             issue_owner = ''
             if github_story_data['assignee'] is not None:
                 issue_owner = (github_story_data['assignee']['login'])
+
                 
             pipeline_name = ''
             if option_pipe:               
@@ -84,10 +252,16 @@ subparsers = parser.add_subparsers()
 
 
 
-report = subparsers.add_parser('report', help='Generate report')
-report.set_defaults(func=do_report)
+report_epic = subparsers.add_parser('report', help='Generate report')
+report_epic.set_defaults(func=do_epic_report)
+
+report_team = subparsers.add_parser('team', help='Generate team report')
+report_team.set_defaults(func=do_team_report)
 
 args = parser.parse_args()
+
+print args
+
 CTM =  (datetime.datetime.now().strftime("%y-%m-%d-%H%M%S"))
 if args.csv:
     csvfile = 'epic-%s-issue-list-%s.csv' % (str(args.e), CTM)
